a77d52400140554222912f2c11dbac32000f9131
rasmus.roudasmaa <rasmus.roudasmaa@WIN-NTV5G055AT0>
2023-01-09 10:14:43 +0200

    Testi commit

---
+++ olio ohjlemointi/olio ohjlemointi/Program.cs
@@ -0,0 +1,15 @@
+﻿wusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace olio_ohjlemointi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
