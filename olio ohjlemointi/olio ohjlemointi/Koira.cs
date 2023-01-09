ce44bbba71c116a8bdfdfde1a2bb8d91e4566037
rasmus.roudasmaa <rasmus.roudasmaa@WIN-NTV5G055AT0>
2023-01-09 12:22:05 +0200

    koira koira woof

---
+++ olio ohjlemointi/olio ohjlemointi/Koira.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace olio_ohjlemointi
+{
+    internal class Koira
+    {
+        public string Nimi;
+        public string Rotu;
+        public int Ikä;
+
+        public Koira(string nimi, string rotu, int ikä)
+        {
+            Nimi = nimi;
+            Rotu = rotu;
+            Ikä = ikä;
+        }
+
+        public void HaeTiedot()
+        {
+            Console.WriteLine("Koiran nimi on " + Nimi);
+            Console.WriteLine("Koiran ikä on " + Ikä);
+            Console.WriteLine("Koiran rotu on " + Rotu);
+        }
+
+        public void Hauku()
+        {
+            Console.WriteLine(Nimi + " woof woof");
+        }
+    }
+}
--- olio ohjlemointi/olio ohjlemointi/Program.cs
+++ olio ohjlemointi/olio ohjlemointi/Program.cs
@@ -1,4 +1,4 @@
-﻿wusing System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,18 @@ namespace olio_ohjlemointi
     {
         static void Main(string[] args)
         {
+            Koira koira1 = new Koira("Rekku", "Mopsi", 2); // luodaan uusi koira olio/instanssi
+
+
+            koira1.HaeTiedot();
+
+            Koira koira2 = new Koira("Ali", "bulldog", 5    );
+
+
+            koira2.HaeTiedot();
+
+            koira1.Hauku();
+            koira2.Hauku();
         }
     }
 }
